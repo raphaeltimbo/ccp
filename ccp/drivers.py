74bc81d658481b32ad64455422bc67ff0cd5105d
raphaeltimbo <raphaelts@petrobras.com.br>
2023-06-19 16:12:43 -0300

    Start adding steam turbine to drivers module

---
+++ ccp/drivers.py
@@ -0,0 +1,19 @@
+"""Module to implement calculation for centrifugal compressor drivers."""
+
+
+class SteamTurbine:
+    def __init__(self, points):
+        """Steam turbine class.
+
+        Parameters
+        ----------
+
+        Examples
+        --------
+        """
+        pass
+
+    def plot_mollier_diagram(self):
+        """Plot Mollier diagram."""
+        # TODO use CoolProp.PropertyPlot to plot Mollier diagram
+        pass
